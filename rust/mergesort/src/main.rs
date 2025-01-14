5422126b15b9a8306d5899602a9f27941a88d946
Anthony T. <ungegrieftfirma@gmail.com>
2025-01-14 14:01:25 +0100

    add mergesort for rust

--- rust/mergesort/src/main.rs
+++ rust/mergesort/src/main.rs
@@ -1,34 +1,62 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let data = generate_data();
+
+    println!("Unsorted data: {:?}", data);
+
+    let start_time = Instant::now();
+
+    let sorted_list = merge_sort(data);
+
+    let elapsed_time = start_time.elapsed();
+
+    println!("Sorted data: {:?}", sorted_list);
+
+    println!("Elapsed time: {:?}", elapsed_time);
 }
 
-fn merge_sort(list: &mut Vec<i64>) {
-    let mut groups: Vec<Vec<&mut i64>> = Vec::new();
+fn generate_data() -> Vec<i64> {
+    let mut data: Vec<i64> = Vec::new();
+
+    for _i in 0..20 {
+        data.push(rand::random_range(1..100));
+    }
+
+    data
+}
+
+fn merge_sort(list: Vec<i64>) -> Vec<i64> {
+    let mut groups: Vec<Vec<i64>> = Vec::new();
 
     for element in list {
         let element_list = vec![element];
         groups.push(element_list);
     }
 
-    let groups_length = groups.len();
+    while groups.len() > 1 {
+        let left_list = groups.remove(0);
+        let right_list = groups.remove(0);
 
-    while groups_length > 1 {
-        merge(&mut groups);
+        groups.push(merge(left_list, right_list));
     }
+
+    groups.remove(0)
 }
 
-fn merge(mut groups: &mut Vec<Vec<&mut i64>>) -> Vec<i64> {
-    let mut left_list: Vec<&mut i64> = groups.remove(0);
-    let mut right_list: Vec<&mut i64> = groups.remove(0);
+fn merge(mut left_list: Vec<i64>, mut right_list: Vec<i64>) -> Vec<i64> {
     let mut temp: Vec<i64> = Vec::new();
 
     while !left_list.is_empty() && !right_list.is_empty() {
         if left_list.get(0) < right_list.get(0) {
-            temp.push(*left_list.remove(0));
+            temp.push(left_list.remove(0));
         } else {
-            temp.push(*right_list.remove(0));
+            temp.push(right_list.remove(0));
         }
     }
+
+    temp.append(&mut left_list);
+    temp.append(&mut right_list);
 
     temp
 }
